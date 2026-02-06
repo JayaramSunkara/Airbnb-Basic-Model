28243a43960744d7b2a306955e4650d010d30bc1
Jayaram Sunkara <jayaram0sunkara@gmail.com>
2026-02-06 14:50:13 +0530

    Connect MongoDB in app.js

--- app.js
+++ app.js
@@ -2,11 +2,10 @@
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // External Modules
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 // Local Modules
 import { page404 } from "./controllers/error.js";
@@ -14,6 +13,11 @@ import storeRouter from "./routes/storeRouter.js";
 import hostRouter from "./routes/hostRouter.js";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -26,7 +30,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(page404);
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to DB: ", error.message);
+  }
+}
+
+startServer();
