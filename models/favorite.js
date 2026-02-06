79404d3114d0cb1a4ae63d002a572dd4c76c0612
Jayaram Sunkara <jayaram0sunkara@gmail.com>
2026-02-06 11:41:56 +0530

    Add favorite model

---
+++ models/favorite.js
@@ -0,0 +1,47 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { readAll } from "./home.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const favoriteDataPath = path.join(__dirname, "../data/favorite.js");
+
+export async function readAllFav() {
+  try {
+    const favoriteHomes = await fs.readFile(favoriteDataPath, "utf8");
+    return JSON.parse(favoriteHomes);
+  } catch (error) {
+    console.error("Error while reading favorites file:", error.message);
+    return [];
+  }
+}
+
+export async function updateFav(homeId) {
+  let favoriteHomes = await readAllFav();
+  if (favoriteHomes.includes(homeId)) return "Home is already marked favourite";
+  favoriteHomes.push(homeId);
+  try {
+    await fs.writeFile(favoriteDataPath, JSON.stringify(favoriteHomes), "utf8");
+    return "Added to favourites";
+  } catch (error) {
+    console.error("Error writing file: ", error.message);
+    return "Failed to update favourites";
+  }
+}
+
+export async function deleteFav(homeId) {
+  let favoriteHomes = await readAllFav();
+  favoriteHomes = favoriteHomes.filter((id) => id !== homeId);
+  try {
+    await fs.writeFile(
+      favoriteDataPath,
+      JSON.stringify(favoriteHomes),
+      "utf-8",
+    );
+  } catch (error) {
+    console.error("Error writing file: ", error.message);
+    return "Failed to delete favourites";
+  }
+}
