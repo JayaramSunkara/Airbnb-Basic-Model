3e207fa347056165772fa8c0fcebbc6dad04d8b7
Jayaram Sunkara <jayaram0sunkara@gmail.com>
2026-02-08 16:56:48 +0530

    Change dbUtil file

--- utils/dbUtil.js
+++ utils/dbUtil.js
@@ -3,18 +3,18 @@ import { MongoClient } from "mongodb";
 let _db;
 
 export async function mongoConnect() {
-  if (db) return db;
+  if (_db) return _db;
 
   const client = new MongoClient(process.env.MONGO_URI);
   await client.connect();
 
-  db = client.db("airbnb");
+  _db = client.db("airbnb");
   console.log("MongoDB connected");
 
-  return db;
+  return _db;
 }
 
-export function getDB() {
-  if (!db) throw new Error("DB not connected");
-  return db;
+export function get_() {
+  if (!_db) throw new Error("DB not connected");
+  return _db;
 }
