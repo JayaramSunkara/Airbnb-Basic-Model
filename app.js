865968fbfbb118f327d1659b62f515c8f980ef0e
Jayaram Sunkara <jayaram0sunkara@gmail.com>
2026-02-10 11:28:19 +0530

    Fix static files middleware

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(express.urlencoded());
 app.use(storeRouter);
 app.use("/host", hostRouter);
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(page404);
 
