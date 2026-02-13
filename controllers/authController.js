2735f428b8a26b92d71f51403791185ba9684421
Jayaram Sunkara <jayaram0sunkara@gmail.com>
2026-02-13 11:33:52 +0530

    Update HomeSchema

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+import { check, validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
+
 export async function getLogin(req, res, next) {
   res.render("auth/login", {
     pageTitle: "Login",
@@ -10,17 +13,120 @@ export async function getLogin(req, res, next) {
 }
 
 export async function postLogin(req, res, next) {
-  console.log(req.body);
-  req.session.isLoggedIn = true;
-  //res.cookie("isLoggedIn", true);
-  //req.isLoggedIn = true;
-  res.redirect("/");
+  const { email, password } = req.body;
 }
+
 export async function postLogout(req, res, next) {
   req.session.destroy(() => {
     res.redirect("/login");
   });
 }
 
-export async function getSignup() {}
-export async function postSignup() {}
+export async function getSignup(req, res, next) {
+  res.render("auth/signup", {
+    pageTitle: "Signup",
+    currentPage: "signup",
+    isLoggedIn: false,
+    errors: [],
+    oldInput: { firstName: "", lastName: "", email: "", userType: "" },
+    user: {},
+  });
+}
+
+export const postSignup = [
+  check("firstName")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First Name should be atleast 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("First Name should contain only alphabets"),
+
+  check("lastName")
+    .matches(/^[A-Za-z\s]*$/)
+    .withMessage("Last Name should contain only alphabets"),
+
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
+
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password should be atleast 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password should contain atleast one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password should contain atleast one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password should contain atleast one number")
+    .matches(/[!@&]/)
+    .withMessage("Password should contain atleast one special character")
+    .trim(),
+
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+
+  check("userType")
+    .notEmpty()
+    .withMessage("Please select a user type")
+    .isIn(["guest", "host"])
+    .withMessage("Invalid user type"),
+
+  check("terms")
+    .notEmpty()
+    .withMessage("Please accept the terms and conditions")
+    .custom((value) => {
+      if (value !== "on") {
+        throw new Error("Please accept the terms and conditions");
+      }
+      return true;
+    }),
+
+  async (req, res) => {
+    const { firstName, lastName, email, password, userType } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Signup",
+        currentPage: "signup",
+        isLoggedIn: false,
+        errors: errors.array().map((err) => err.msg),
+        oldInput: { firstName, lastName, email, password, userType },
+        user: {},
+      });
+    }
+
+    try {
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const user = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        userType,
+      });
+
+      await user.save();
+
+      res.redirect("/login");
+    } catch (err) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Signup",
+        currentPage: "signup",
+        isLoggedIn: false,
+        errors: [err.message],
+        oldInput: { firstName, lastName, email, userType },
+        user: {},
+      });
+    }
+  },
+];
